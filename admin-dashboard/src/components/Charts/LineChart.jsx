fa6835ca4243ffa13baeec8599eb68a6057d824e
Elias Fernandes <eliasneto0014@gmail.com>
2023-01-13 10:42:46 -0300

    improving the routes

--- admin-dashboard/src/components/Charts/LineChart.jsx
+++ admin-dashboard/src/components/Charts/LineChart.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const LineCharts = () => {
+const LineChart = () => {
     return(
         <div>LineCharts</div>
     )
 }
-export default LineCharts;
+export default LineChart;
