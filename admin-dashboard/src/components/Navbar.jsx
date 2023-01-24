808c5bbd798eb9700cb271f0087d3daba224f0b2
Elias Fernandes <eliasneto0014@gmail.com>
2023-01-24 09:58:04 -0300

    creating navbar icons and avatar

--- admin-dashboard/src/components/Navbar.jsx
+++ admin-dashboard/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect}from 'react';
+import React, {useEffect,}from 'react';
 import {AiOutlineMenu} from 'react-icons/ai';
 import {FiShoppingCart} from 'react-icons/fi';
 import {BsChatLeft} from "react-icons/bs";
@@ -25,13 +25,39 @@ const NavButton = ({title,customFunc,icon,color,dotColor}) => (
     </TooltipComponent>
 )
 const Navbar = () => {
-    const {activeMenu, setActiveMenu} = useStateContext();
+    const {activeMenu, setActiveMenu,handleClick} = useStateContext();
     return(
         <div className="flex justify-between p-2 md:mx-6 relative">
             <NavButton title="Menu" customFunc={() =>
                 setActiveMenu((prevActiveMenu) =>
                     !activeMenu)} color= "blue" icon={<AiOutlineMenu/>}/>
+            <div className="flex">
+                <NavButton dotColor="#03C9D7" title="Cart" customFunc={() =>
+                    handleClick('cart')} color= "blue" icon={<FiShoppingCart/>}/>
+
+                <NavButton  dotColor="#03C9D7" title="Chat" customFunc={() =>
+                    handleClick('chat')} color= "blue" icon={<BsChatLeft/>}/>
+
+                <NavButton  dotColor="#03C9D7" title="Notifications" customFunc={() =>
+                    handleClick('Notifications')} color= "blue" icon={<RiNotification3Line/>}/>
+
+                <TooltipComponent postition="BottomCenter" content="Profile">
+                    <div className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+                    onClick={() => handleClick('userProfile') }>
+                        <img  className="rounded-full w-8 h-8" src={avatar}/>
+                        <p>
+                            <span className="text-gray-400 text-14">Hi,</span>
+                            <span className="text-gray-400 font-bold ml-1 text-14">Michael</span>
+                        </p>
+                        <MdKeyboardArrowDown  className="text-gray-400 text-14"/>
+
+                    </div>
+
+                </TooltipComponent>
+
+            </div>
         </div>
+
     )
 }
 export default Navbar;
