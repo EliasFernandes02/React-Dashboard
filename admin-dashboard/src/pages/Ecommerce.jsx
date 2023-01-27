73506cb31430476a9ed393b517fa29788549aa03
EliasFernandes02 <eliasneto0014@gmail.com>
2023-01-27 16:16:37 -0300

    creating sections from banner

--- admin-dashboard/src/pages/Ecommerce.jsx
+++ admin-dashboard/src/pages/Ecommerce.jsx
@@ -11,7 +11,6 @@ const Ecommerce = () => {
             <div className="flex flex-wrap lg:flex-nowrap justify-center">
                 <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg  rounded-xl w-full lg:w-90  pt-9 m-3 bg-hero-pattern  bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(${Banner2})` }}>
                   
-
                     <div className='flex justify-between items-center'>
                         <div>
                             <p className='font-bold text-gray-400'>Earnings</p>
@@ -27,10 +26,31 @@ const Ecommerce = () => {
                         borderRadius="10px"
                         size="md"/>
                     </div>
+                </div>
+            </div>
+                <div className="flex m-3 flex-wrap justify-center gap-5 items-center">
+                    {earningData.map((item) => (
+                        <div key={item.title}
+                            className="bg-white dark:text-gray-200
+                            dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl">
+                            <button  type="button"style={{color:item.iconColor,
+                                backgroundColor:item.iconBg}}
+                                className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl">
+                                    {item.icon}
+                            </button>
+                            <p className="mt-3">
+                                <span className="text-lg font-semibold"> {item.amount}</span>
+                                <span  style={{color:"purple"}}className={`text-sm text-${item.pcColor} ml-2`}> {item.percentage}</span>
+                            </p>
+                            <p className="text-sm text-gray-400 mt-1">{item.title}</p>
+
+                        </div>
+                    ))}
 
                 </div>
-
-            </div>
+                <div>
+                    
+                </div>
         </div>
     )
 }
