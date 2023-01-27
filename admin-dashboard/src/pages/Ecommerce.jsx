d721eac98889ee0fa640a1bd1c32e59a3813178b
EliasFernandes02 <eliasneto0014@gmail.com>
2023-01-27 15:48:37 -0300

    implementing the banner

--- admin-dashboard/src/pages/Ecommerce.jsx
+++ admin-dashboard/src/pages/Ecommerce.jsx
@@ -9,8 +9,24 @@ const Ecommerce = () => {
     return(
         <div className="mt-12">
             <div className="flex flex-wrap lg:flex-nowrap justify-center">
-                <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg  rounded-xl w-full lg:w-90  pt-9 m-3 bg-hero-pattern  bg-no-repeat bg-cover bg-center">
-                    <img src={Banner2} style={{height:"350px", width:"100%"}} />
+                <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg  rounded-xl w-full lg:w-90  pt-9 m-3 bg-hero-pattern  bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(${Banner2})` }}>
+                  
+
+                    <div className='flex justify-between items-center'>
+                        <div>
+                            <p className='font-bold text-gray-400'>Earnings</p>
+                            <p className='text-2xl'>$690.099,90</p>
+                        </div>
+
+                    </div>
+                    <div className='mt-6'>
+                        <Button
+                        color="white"
+                        bgColor="blue"
+                        text="Donwload"
+                        borderRadius="10px"
+                        size="md"/>
+                    </div>
 
                 </div>
 
