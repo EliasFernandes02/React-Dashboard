1640e211dff254c34362a433395affbd5aa9430f
EliasFernandes02 <eliasneto0014@gmail.com>
2023-02-22 15:15:50 -0300

    add budget

--- admin-dashboard/src/pages/Ecommerce.jsx
+++ admin-dashboard/src/pages/Ecommerce.jsx
@@ -48,7 +48,36 @@ const Ecommerce = () => {
                     ))}
 
                 </div>
-                <div>
+                  <div className='flex gap-10 flex-wrap justify-center'>
+                    <div className='bg-white
+                    dark:text-gray-200
+                    dark:bg-secondary-dark-bg m-3 p-2
+                    rounded-2xl md:w-780'>
+                        <div className='flex justify-between'>
+                                <p className='font-semibold text-xl'>Updates</p>
+                                <div className='flex items-center gap-4'>
+                                    <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'>
+                                        <span><GoPrimitiveDot/></span>
+                                        <span>Expense</span>
+                                    </p>
+
+                                    <p className='flex items-center gap-2 text-green-400 hover:drop-shadow-xl'>
+                                        <span><GoPrimitiveDot/></span>
+                                        <span>Budget</span>
+                                    </p>
+                                        
+                                </div>
+                        </div>
+                        <div className='mt-10 flex gap-10 flex-wrap justify-center'>
+                            <div className='border-r-1 border-color m-4 pr-10'>
+                                <div>
+                                    <p></p>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div> 
                     
                 </div>
         </div>
